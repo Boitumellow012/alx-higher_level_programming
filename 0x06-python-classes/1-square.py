d76daaab05b4b977eb6e6642e95be4c1db59f1e7
Boitumellow012 <brakobeloa@gmail.com>
2023-08-29 17:13:50 +0000

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a  class with the name Square"""
+
+
+class Square:
+    """Defines an empty square."""
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
+
+         self.__size = size    
+~           
